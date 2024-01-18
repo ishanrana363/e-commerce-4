8323b9619381d0147cd7f3ece28bc18a2038d386
ishanrana363 <ishanrana363@gmail.com>
2024-01-18 12:43:22 +0600

    products details done

---
+++ src/models/productdetailsModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const {Schema,model} = mongoose;
+
+
+const productdetailsSchema = new Schema({
+    img1 : {
+        type : String,
+        required : true
+    },
+    img2 : {
+        type : String,
+        required : true
+    },
+    img3 : {
+        type : String,
+        required : true
+    },
+    img4 : {
+        type : String,
+        required : true
+    },
+    img5 : {
+        type : String,
+    },
+    img6 : {
+        type : String,
+    },
+    img7 : {
+        type : String,
+    },
+    img8 : {
+        type : String,
+    },
+    des : {
+        type : String,
+        required : true
+    },
+    color : {
+        type : String,
+        required : true
+    },
+    size : {
+        type : String,
+        required : true
+    },
+    productID : {
+        type : new mongoose.Types.ObjectId(),
+        required : true
+    }
+},{timestamps:true,versionKey:false});
+
+const productdetailsModel = model("productdetails",productdetailsSchema);
+
+module.exports = productdetailsModel;
