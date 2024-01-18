3b6377b4290f3e1f88cc8867cb5bb2230f93071e
ishanrana363 <ishanrana363@gmail.com>
2024-01-18 14:45:57 +0600

    invoice productmodel done

---
+++ src/models/invoiceproductsModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const {Schema,model} = mongoose;
+
+const invoiceproductsSchema = new Schema({
+    userID : {
+        type : new mongoose.Types.ObjectId(),
+        required : true
+    },
+    invoiceID : {
+        type : new mongoose.Types.ObjectId(),
+        required : true
+    },
+    productID : {
+        type : new mongoose.Types.ObjectId(),
+        required : true
+    },
+    qty : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : String,
+        required : true
+    },
+    color : {
+        type : String,
+        required : true
+    },
+    size : {
+        type : String,
+        required : true
+    }
+},{timestamps:true,versionKey:false});
+
+
+const invoiceproductsModel = model("invoiceproducts",invoiceproductsSchema);
+
+module.exports = invoiceproductsModel;
